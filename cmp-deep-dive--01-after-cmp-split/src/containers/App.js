cb2ce260c7736e681bf3edbebe0f6f5c099928ca
Marcelo Amador <marcelojaf@gmail.com>
2018-06-14 21:51:15 -0400

    Lecture 84

--- cmp-deep-dive--01-after-cmp-split/src/containers/App.js
+++ cmp-deep-dive--01-after-cmp-split/src/containers/App.js
@@ -5,14 +5,26 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
-  state = {
-    persons: [
-      { id: 'asfa1', name: 'Max', age: 28 },
-      { id: 'vasdf1', name: 'Manu', age: 29 },
-      { id: 'asdf11', name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some other value',
-    showPersons: false
+  constructor(props){
+    super(props)
+    console.log('[App.js] Inside Constructor', props)
+    this.state = {
+      persons: [
+        { id: 'asfa1', name: 'Max', age: 28 },
+        { id: 'vasdf1', name: 'Manu', age: 29 },
+        { id: 'asdf11', name: 'Stephanie', age: 26 }
+      ],
+      otherState: 'some other value',
+      showPersons: false
+    }
+  }
+
+  componentWillMount() {
+    console.log('[App.js] Inside componentWillMount()')
+  }
+
+  componentDidMount() {
+    console.log('[App.js] Inside componentDidMount()')
   }
 
   nameChangedHandler = ( event, id ) => {
@@ -47,6 +59,7 @@ class App extends Component {
   }
 
   render () {
+    console.log('[App.js] Inside render()')
     let persons = null;
 
     if ( this.state.showPersons ) {
