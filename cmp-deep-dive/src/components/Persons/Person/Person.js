5c623ccc53e6de2a0dbc9c86cd2b9d9e86361f01
Marcelo Amador <marcelojaf@gmail.com>
2018-06-18 20:18:43 -0400

    react 16.3

--- cmp-deep-dive/src/components/Persons/Person/Person.js
+++ cmp-deep-dive/src/components/Persons/Person/Person.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types'
 
 import classes from './Person.css'
 import withClass from '../../../hoc/withClass'
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/ReactAux'
+import {AuthContext} from '../../../containers/App'
 
 class Person extends Component {
   constructor (props) {
     super(props)
     console.log('[Person.js] Inside Constructor', props)
+    this.inputElement = React.createRef()
   }
 
   componentWillMount () {
@@ -18,19 +20,26 @@ class Person extends Component {
   componentDidMount () {
     console.log('[Person.js] Inside componentDidMount()')
     if (this.props.position === 0) {
-      this.inputElement.focus()
-      this.inputElement.select()
+      this.inputElement.current.focus()
     }
+  }
+
+  focus () {
+    this.inputElement.current.focus()
+    this.inputElement.current.select()
   }
 
   render () {
     console.log('[Person.js] Inside render()')
     return (
       <Aux>
+        <AuthContext.Consumer>
+          {auth => auth ? <p>I'm authenticated!</p> : null}
+        </AuthContext.Consumer>
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input
-          ref={(inp) => { this.inputElement = inp }}
+          ref={this.inputElement}
           type='text'
           onChange={this.props.changed}
           value={this.props.name} />
--- cmp-deep-dive/src/containers/App.js
+++ cmp-deep-dive/src/containers/App.js
@@ -6,6 +6,8 @@ import Cockpit from '../components/Cockpit/Cockpit'
 import Aux from '../hoc/ReactAux'
 import withClass from '../hoc/withClass'
 
+export const AuthContext = React.createContext(false)
+
 class App extends PureComponent {
   constructor(props) {
     super(props)
@@ -18,7 +20,8 @@ class App extends PureComponent {
       ],
       otherState: 'some other value',
       showPersons: false,
-      toggleClicked: 0
+      toggleClicked: 0,
+      authenticated: false
     }
   }
 
@@ -38,6 +41,16 @@ class App extends PureComponent {
 
   componentWillUpdate(nextProps, nextState) {
     console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState)
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log('[UPDATE App.js] Inside getDerivedStateFromProps', nextProps, prevState)
+
+    return prevState
+  }
+
+  getSnapshotBeforeUpdate(){
+    console.log('[UPDATE App.js] Inside getSnapshotBeforeUpdate')
   }
 
   componentDidUpdate() {
@@ -80,6 +93,10 @@ class App extends PureComponent {
     });
   }
 
+  loginHandler = () => {
+    this.setState({ authenticated: true })
+  }
+
   render() {
     console.log('[App.js] Inside render()')
     let persons = null;
@@ -98,8 +115,11 @@ class App extends PureComponent {
           appTitle={this.props.title}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
+          login={this.loginHandler}
           clicked={this.togglePersonsHandler} />
-        {persons}
+        <AuthContext.Provider value={this.state.authenticated}>
+          {persons}
+        </AuthContext.Provider>
       </Aux>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
