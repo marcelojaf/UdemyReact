e0bcef81633d168d7808b0e3fe71be555dd07cdf
Marcelo Amador <marcelo@redbitdev.com>
2018-06-27 20:21:40 -0400

    Added toolbar and navigation items. Also, finalized project

--- burger-project/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-project/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,36 @@
-import React from 'react'
-import Aux from '../../../hoc/Auxiliar'
+import React, { Component } from 'react'
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return (
-        <li key={igKey}>
-          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
-        </li>
-      )
-    })
+import Aux from '../../../hoc/Auxiliar/Auxiliar'
+import Button from '../../UI/Button/Button'
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p>Continue to Checkout?</p>
-    </Aux>
-  )
+class OrderSummary extends Component {
+  componentWillUpdate () {
+    console.log('[OrderSummary]')
+  }
+
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+          </li>
+        )
+      })
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button btnType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+    )
+  }
 }
 
-export default orderSummary
+export default OrderSummary
